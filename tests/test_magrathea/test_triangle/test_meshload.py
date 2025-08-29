33b422950c0d07fa2568178c07139fbc247f6700
kwan3217 <kwan3217@gmail.com>
2025-08-29 16:00:28 -0600

    Added loading of 3MF files, the best export format for openscad. This properly preserves colors.

---
+++ tests/test_magrathea/test_triangle/test_meshload.py
@@ -0,0 +1,10 @@
+"""
+Describe purpose of this script here
+
+Created: 8/28/25
+"""
+from magrathea.triangle.meshload import Mesh
+
+
+def test_3mf():
+    voyager=Mesh("data/output/mesh/voyager.3mf")
